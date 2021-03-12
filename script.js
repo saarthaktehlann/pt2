f6a48a4478d6a708dd3b2a49607084a571f0fb5a
saarthaktehlann <55968567+saarthaktehlann@users.noreply.github.com>
2021-03-12 22:36:01 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+var typed = new Typed(".typing",{
+  strings: ["Travel More", "Attend more concerts", "Go to parties"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop:true,
+  showCursor: false
+});
+
+
+var typed1 = new Typed(".typing1",{
+  strings: ["Host a party", "Try different cuisines", "Go GoKarting"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop:true,
+  showCursor: false
+});
+
+
+var typed2 = new Typed(".typing2",{
+  strings: ["Learn to swim", "Do a roadtrip"],
+  typeSpeed:100,
+  backSpeed:60,
+  loop:true,
+  showCursor: false
+});
